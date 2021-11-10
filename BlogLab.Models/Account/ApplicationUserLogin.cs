63f14033397a7e107523d6cdd7545a04b10f2209
Afreen Haider <haiderafreen2016@gmail.com>
2021-11-10 12:04:13 +0530

    Models project complete. Repository project created with Account Repository.

---
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage ="Username is Required")]
+        [MinLength(5, ErrorMessage ="Must be 5-20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be 5-20 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(10, ErrorMessage = "Must be 10-50 characters")]
+        [MaxLength(50, ErrorMessage = "Must be 10-50 characters")]
+        public string Password { get; set; }
+    }
+}
---
+++ BlogLab.Models/BlogComment/BlogCommentCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogLab.Models.BlogComment
+{
+    public class BlogCommentCreate
+    {
+        public int BlogCommentId { get; set; }
+
+        public int? ParentBlogCommentId { get; set; }
+
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Content is Required")]
+        [MinLength(10, ErrorMessage = "Must 10-300 characters")]
+        [MaxLength(300, ErrorMessage = "Must 10-300 characters")]
+        public string Content { get; set; }
+    }
+}
