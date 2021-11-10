c1e41e560c8c7f410080ffa18563a81b5c4cf4a8
Afreen Haider <haiderafreen2016@gmail.com>
2021-11-10 15:37:20 +0530

    Services project completed

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,3 +1,4 @@
+using BlogLab.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,9 +13,9 @@ namespace BlogLab.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            Configuration = config;
         }
 
         public IConfiguration Configuration { get; }
@@ -22,7 +23,9 @@ namespace BlogLab.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
